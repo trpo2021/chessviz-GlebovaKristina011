3e865263183ffe2eb90c6a22b10da549adf70c12
GlebovaKristina011 <78631803+GlebovaKristina011@users.noreply.github.com>
2021-03-06 10:45:10 +0700

    Update chess.cpp

--- chess.cpp
+++ chess.cpp
@@ -2,23 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printboard(char board[9][9]) {
-  int i, z;
-  for (i = 0; i < 8; i++) {
-    printf("%d| ", i + 1);
-    for (z = 0; z < 8; z++) {
-      printf("%c ", board[i][z]);
+void printboard(char board[9][9])
+{
+    int i, z;
+    for (i = 0; i < 8; i++) {
+        printf("%d| ", i + 1);
+        for (z = 0; z < 8; z++) {
+            printf("%c ", board[i][z]);
+        }
+        printf("\n");
     }
-    printf("\n");
-  }
-  printf("  -----------------  \n   a b c d e f g h");
+    printf("  -----------------  \n   a b c d e f g h");
 }
 
-int main() {
-  char board[9][9] = {"rnbqkbnr", "pppppppp", "        ", "        ",
-                      "        ", "        ", "PPPPPPPP", "RNBQKBNR"};
+int main()
+{
+    char board[9][9] = { "rnbqkbnr", "pppppppp", "        ", "        ",
+        "        ", "        ", "PPPPPPPP", "RNBQKBNR" };
 
-  printboard(board);
-  printf("\n");
-  return 0;
+    printboard(board);
+    printf("\n");
+    return 0;
 }
