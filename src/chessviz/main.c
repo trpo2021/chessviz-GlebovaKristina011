6e7a665ed8e6d54c07d0e8ab448c51c109204d9d
GlebovaKristina011 <78631803+GlebovaKristina011@users.noreply.github.com>
2021-03-27 22:32:42 +0700

    Update main.c

--- src/chessviz/main.c
+++ src/chessviz/main.c
@@ -3,13 +3,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main()
-{
-    char board[11][11] = { "rnbqkbnr", "pppppppp", "        ", "        ",
-        "        ", "        ", "PPPPPPPP", "RNBQKBNR" };
-    printboard(board);
-    while (1) {
-        hod(board);
-    }
-    return 0;
+int main() {
+  char board[11][11] = {"rnbqkbnr", "pppppppp", "        ", "        ",
+                        "        ", "        ", "PPPPPPPP", "RNBQKBNR"};
+  printboard(board);
+  while (1) {
+    hod(board);
+  }
+  return 0;
 }
