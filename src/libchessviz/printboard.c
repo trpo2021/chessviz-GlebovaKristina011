3f37bd6b091d69d4448d33139d8c84956d91600e
GlebovaKristina011 <78631803+GlebovaKristina011@users.noreply.github.com>
2021-03-27 22:41:55 +0700

    Update printboard.c

--- src/libchessviz/printboard.c
+++ src/libchessviz/printboard.c
@@ -1,15 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void printboard(char chessboard1[11][11]) {
-  int i, z;
-  for (i = 0; i < 8; i++) {
-    printf("%d|", i + 1);
-    for (z = 0; z < 8; z++) {
-      printf("%c ", chessboard1[i][z]);
+void printboard(char chessboard1[11][11])
+{
+    int i, z;
+    for (i = 0; i < 8; i++) {
+        printf("%d|", i + 1);
+        for (z = 0; z < 8; z++) {
+            printf("%c ", chessboard1[i][z]);
+        }
+        printf("\n");
     }
+    printf("  _______________  \n  a b c d e f g h");
     printf("\n");
-  }
-  printf("  _______________  \n  a b c d e f g h");
-  printf("\n");
 }
